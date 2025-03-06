906ca075e57f89af7c266aa5b55a898cf6375b0c
shenjingnan <sjn.code@gmail.com>
2025-03-06 21:13:22 +0800

    fix

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
+import { fileURLToPath } from 'url';
 
-// https://vite.dev/config/
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const projectRoot = path.resolve(__dirname, '../..'); // 回到项目根目录
 export default defineConfig({
+  root: __dirname,
+  build: {
+    outDir: path.resolve(projectRoot, 'dist/apps/frontend'),
+    emptyOutDir: true,
+  },
   plugins: [react()],
   resolve: {
     alias: {
